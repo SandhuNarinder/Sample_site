d9484f593b59cb3e06df54eabadd314e9645227c
SandhuNarinder <57005315+SandhuNarinder@users.noreply.github.com>
2020-03-26 08:37:49 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+ <!DOCTYPE html>
+<html>
+    <head>
+        <title>Home</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="main.css">
+        
+        
+    </head>
+    
+    <body>
+        <div id="wrapper">
+        
+            <nav>
+                <ul>
+                    <li><a href="index.html" class="active">Home</a></li>
+                    <li><a href="photo.html">Photo</a></li>
+                    <li><a href="about-us.html">About Us</a></li>
+                    <li><a href="contact.html">Contact Us</a></li>
+                </ul>
+            </nav>
+            
+            <main>
+                
+                <div class="section-sub">
+                    <h1>SIGN UP</h1>
+                    <input type="text" name="username" placeholder="username"/><br/>
+            <input type="password" name="password" placeholder="password"/><br/>
+    <input type="submit" name="signup-button" value="signup"/><br/>
+            
+                </div>
+            </main>
+            
+            <footer>
+                <h4>&copy; Untitled. All rights reserved.</h4>
+            </footer>
+        </div>
+        
+       
+    </body>
+</html>
